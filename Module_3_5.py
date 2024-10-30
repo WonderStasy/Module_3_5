f151bcda964e6ca085d52cd650a64227100f20dc
WonderStasy <stanislavflowers@ya.ru>
2024-10-30 21:45:01 +0300

    Update Module_3_5.py

--- Module_3_5.py
+++ Module_3_5.py
@@ -3,9 +3,10 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
 
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+        result = first * get_multiplied_digits(int(str_number[1:]))
+        return result
     else:
-        return first
+        return int(str_number)
 
 result = get_multiplied_digits(40203)
 print(result)
